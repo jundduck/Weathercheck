ce2df641436edc49eb02dd4c0d3d7be1cf9f6b1f
이준석 <ryan0826@naver.com>
2024-06-19 20:00:21 +0900

    Initial Commit

---
+++ Weathercheck/WeathercheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathercheckApp.swift
+//  Weathercheck
+//
+//  Created by snlcom on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WeathercheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
